05f7eb5e0de6f2118ac2b0eaf9d3b3a57dc60db4
GrgijKD <svidpustka@gmail.com>
2025-04-19 14:57:54 +0300

    etap 2.1

---
+++ src/ComputerShopAPIWebApp/ComputerShopAPIWebApp/Models/Computer.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputerShopAPIWebApp.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
+        [Display(Name = "Тип")]
+        public required string Type { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
+        [Display(Name = "Фірма")]
+        public required string Brand { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
+        [Display(Name = "Діагональ екрану")]
+        public required string ScreenSize { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
+        [Display(Name = "Роздільна здатність")]
+        public required string Resolution { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
+        [Display(Name = "Процесор")]
+        public required string Processor { get; set; }
+
+        [Display(Name = "Дискретна відеокарта")]
+        public string? GPU { get; set; }
+
+        [Display(Name = "Обсяг відеопам'яті")]
+        public string? VRAM { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
+        [Display(Name = "Обсяг пам'яті")]
+        public required string Storage { get; set; }
+
+        [Required(ErrorMessage = "Поле не повинно бути порожнім")]
+        [Display(Name = "Обсяг оперативної пам'яті")]
+        public required string RAM { get; set; }
+    }
+}
---
+++ src/ComputerShopAPIWebApp/ComputerShopAPIWebApp/Models/ShopAPIContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ComputerShopAPIWebApp.Models
+{
+    public class ShopAPIContext : DbContext
+    {
+        public virtual DbSet<Computer> Computers { get; set; }
+
+        public ShopAPIContext(DbContextOptions<ShopAPIContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
